50fe61aeb02a38c53d95fa7d53dd12aa5fa96719
Todd <toddnordquist144@gmail.com>
2023-05-23 21:44:02 -0600

    Remove unused imports.

--- src/jtable/TableSortDemo.java
+++ src/jtable/TableSortDemo.java
@@ -5,9 +5,6 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
